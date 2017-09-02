2d15367c7d812029bbac020fa123e9cf3bb76d16
Maru <matheus@itsmaru.com>
2017-09-02 18:24:31 -0300

    Definitions for Action type and aspect;

--- src/servers/Server_Zone/Action/Action.h
+++ src/servers/Server_Zone/Action/Action.h
@@ -6,6 +6,32 @@
 
 namespace Core { namespace Action {
 
+   enum struct ActionAspect : uint32_t
+   {
+      None       = 0,   // Doesn't imply unaspected
+      Fire       = 1,
+      Ice        = 2,
+      Wind       = 3,
+      Stone      = 4,
+      Lightning  = 5,
+      Water      = 6,
+      Unaspected = 7    // Doesn't imply magical unaspected damage - could be unaspected physical
+   };
+
+   enum struct ActionType : int16_t
+   {
+      Physical   = -1,
+      Unknown_0  = 0, // Very likely actions that only deals with status effects and nothing else
+      Unknown_1  = 1, // Related to actions that deal with player movement (knockbacks, gapclosers etc)
+      Unknown_2  = 2, // Possibly attacks that bypass calculation (deal raw damage)
+      Unknown_3  = 3, // Possibly AoEs without marker
+      Unknown_4  = 4,
+      Magical    = 5,
+      Unknown_6  = 6, // Possibly breath, eye & song attacks
+      Unknown_7  = 7,
+      LimitBreak = 8,
+   };
+
    class Action 
    {
 
