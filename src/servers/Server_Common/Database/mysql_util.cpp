1031d66522eb139f0aa323a286d15893636f09cd
Mordred <ffxivsapphire@gmail.com>
2017-09-19 00:20:45 +0200

    Fix linux build, needs some love for the warnings

--- src/servers/Server_Common/Database/mysql_util.cpp
+++ src/servers/Server_Common/Database/mysql_util.cpp
@@ -1,5 +1,34 @@
 #include "mysql_util.h"
 
+long double strtold(const char *nptr, char **endptr)
+{
+   /*
+  * Experienced odd compilation errors on one of windows build hosts -
+  * cmake reported there is strold function. Since double and long double on windows
+  * are of the same size - we are using strtod on those platforms regardless
+  * to the HAVE_FUNCTION_STRTOLD value
+  */
+#ifdef _WIN32
+   return ::strtod(nptr, endptr);
+#else
+   # ifndef HAVE_FUNCTION_STRTOLD
+  return ::strtod(nptr, endptr);
+# else
+#  if defined(__hpux) && defined(_LONG_DOUBLE)
+  union {
+    long_double l_d;
+    long double ld;
+  } u;
+  u.l_d = ::strtold( nptr, endptr);
+  return u.ld;
+#  else
+  return ::strtold(nptr, endptr);
+#  endif
+# endif
+#endif
+
+}
+
 long double strtonum( const std::string &str, int radix )
 {
    typedef std::istreambuf_iterator< char > iter_t;
