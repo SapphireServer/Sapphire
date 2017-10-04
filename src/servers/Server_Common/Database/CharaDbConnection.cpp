8bbad4ea3b70661076711c7774315caaf7dd8509
Mordred <ffxivsapphire@gmail.com>
2017-10-04 23:28:38 +0200

    Player loading moved over to new DB system

--- src/servers/Server_Common/Database/CharaDbConnection.cpp
+++ src/servers/Server_Common/Database/CharaDbConnection.cpp
@@ -21,4 +21,17 @@ void Core::Db::CharaDbConnection::doPrepareStatements()
 
    prepareStatement( CHAR_INS_TEST, "INSERT INTO zoneservers ( id, ip, port ) VALUES ( ?, ?, ?);", CONNECTION_BOTH );
 
+   prepareStatement( CHAR_SEL_LOAD, "SELECT c.Name, c.PrimaryTerritoryId, c.Hp, c.Mp, c.Gp, c.Mode, "
+                                           "c.Pos_0_0, c.Pos_0_1, c.Pos_0_2, c.Pos_0_3, c.FirstLogin, "
+                                           "c.Customize, c.ModelMainWeapon, c.ModelSubWeapon, c.ModelEquip, "
+                                           "cd.GuardianDeity, cd.BirthDay, cd.BirthMonth, cd.Status, cd.Class, "
+                                           "cd.Homepoint, cd.HowTo, c.ContentId, c.Voice, cd.QuestCompleteFlags, "
+                                           "cd.QuestTracking, c.IsNewGame, cd.Aetheryte, cd.unlocks, cd.Discovery, "
+                                           "cd.StartTown, cd.TotalPlayTime, c.IsNewAdventurer, cd.GrandCompany, "
+                                           "cd.GrandCompanyRank, cd.CFPenaltyUntil, cd.OpeningSequence, cd.GMRank "
+                                           "FROM charabase AS c "
+                                           " INNER JOIN charadetail AS cd "
+                                           " ON c.CharacterId = cd.CharacterId "
+                                           "WHERE c.CharacterId = ?;", CONNECTION_SYNCH );
+
 }
