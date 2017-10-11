964fdd50e39727f455127e579f5a888657c1b9b7
Mordred <30826167+SapphireMordred@users.noreply.github.com>
2017-10-11 13:18:05 +0200

    hateList opcode updated

--- src/servers/Server_Common/Network/PacketDef/Ipcs.h
+++ src/servers/Server_Common/Network/PacketDef/Ipcs.h
@@ -77,7 +77,7 @@ namespace Packets {
       PlayerSpawn                = 0x011C, // updated 4.1
       NpcSpawn                   = 0x011D, // updated 4.1
       ActorMove                  = 0x011E, // updated 4.1
-      HateList                   = 0x011A,
+      HateList                   = 0x0126, // updated 4.1
       UpdateClassInfo            = 0x011D,
       InitUI                     = 0x012B, // updated 4.1
       ActorOwner                 = 0x012D, // updated 4.1
