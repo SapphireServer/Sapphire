090001a1b58dd0f357ed8cc1a4e5bf191eb4069c
Mordred Admin <gogs@fake.local>
2017-12-05 11:43:31 +0100

    Also should not be comitted

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -1,7 +1,7 @@
 
 if(UNIX)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
+#  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
 else()
   add_definitions(-D_WIN32_WINNT=0x601)
   add_definitions(-D_CRT_SECURE_NO_WARNINGS)
