6ff0e7d4145b0f9836495a617a419e5c7e210510
GokuWeedLord <adam@notadam.me>
2017-12-10 02:13:54 +1100

    script api changes, fix indenting, some broken cmake shit

---
+++ scripts/native/ScriptObject.h
@@ -0,0 +1,9 @@
+#ifndef SAPPHIRE_SCRIPTOBJECT_H
+#define SAPPHIRE_SCRIPTOBJECT_H
+
+#include <Server_Zone/Script/NativeScriptApi.h>
+#include <Server_Zone/Actor/Actor.h>
+#include <Server_Zone/Actor/Player.h>
+
+
+#endif //SAPPHIRE_SCRIPTOBJECT_H
