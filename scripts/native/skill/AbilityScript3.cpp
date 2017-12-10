2474881788bba8ceffef8dd93fa5c036365a64b7
GokuWeedLord <adam@notadam.me>
2017-12-10 22:04:46 +1100

    scripts build to their own libs now

--- scripts/native/skill/AbilityScript3.cpp
+++ scripts/native/skill/AbilityScript3.cpp
@@ -1,15 +1,15 @@
-//#include "../ScriptObject.h"
-//
-//class ActionScript3 : public ActionScript
-//{
-//public:
-//    ActionScript3() : ActionScript( "AbilityScript3", 3 )
-//    {}
-//
-//    virtual void onCastFinish( Core::Entity::Player player, Core::Entity::ActorPtr targetActor )
-//    {
-//       player.addStatusEffectByIdIfNotExist( 50, 20000, player, 30 );
-//    }
-//};
-//
-//EXPORT_ACTIONSCRIPT( ActionScript3 )
+#include "../ScriptObject.h"
+
+class ActionScript3 : public ActionScript
+{
+public:
+    ActionScript3() : ActionScript( "ActionScript3", 3 )
+    {}
+
+    virtual void onCastFinish( Core::Entity::Player player, Core::Entity::ActorPtr targetActor )
+    {
+       player.addStatusEffectByIdIfNotExist( 50, 20000, player, 30 );
+    }
+};
+
+EXPORT_ACTIONSCRIPT( ActionScript3 )
--- scripts/native/statuseffect/StatusEffect50.cpp
+++ scripts/native/statuseffect/StatusEffect50.cpp
@@ -6,11 +6,11 @@ public:
    StatusEffect50() : StatusEffectScript( "StatusEffect50", 50 )
    {}
 
-//   virtual void onTick( Core::Entity::ActorPtr actor )
-//   {
-//      if( actor->isPlayer() )
-//         actor->getAsPlayer()->sendDebug( "tick tock bitch" );
-//   }
+   virtual void onTick( Core::Entity::ActorPtr actor )
+   {
+      if( actor->isPlayer() )
+         actor->getAsPlayer()->sendDebug( "tick tock bitch" );
+   }
 };
 
 EXPORT_STATUSEFFECTSCRIPT( StatusEffect50 )
