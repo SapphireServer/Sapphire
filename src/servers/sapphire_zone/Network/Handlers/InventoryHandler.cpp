4023fa103f77fab8406b5c3225bf90be0592e488
NotAdam <adam@notadam.me>
2018-06-12 12:46:05 +0000

    #31 & #314: Splitting and stacking items in inventory now works

--- src/servers/sapphire_zone/Network/Handlers/InventoryHandler.cpp
+++ src/servers/sapphire_zone/Network/Handlers/InventoryHandler.cpp
@@ -47,6 +47,7 @@ void Core::Network::GameConnection::inventoryModifyHandler( const Packets::GameP
    uint8_t toSlot = inPacket.getValAt< uint8_t >( 0x44 );
    uint16_t fromContainer = inPacket.getValAt< uint16_t >( 0x2C );
    uint16_t toContainer = inPacket.getValAt< uint16_t >( 0x40 );
+   uint16_t splitCount = inPacket.getValAt< uint16_t >( 0x48 );
 
    ZoneChannelPacket< FFXIVIpcInventoryActionAck > ackPacket( player.getId() );
    ackPacket.data().sequence = seq;
@@ -83,13 +84,13 @@ void Core::Network::GameConnection::inventoryModifyHandler( const Packets::GameP
 
       case InventoryOperation::Merge: // merge stack action
       {
-
+         player.getInventory()->mergeItem( fromContainer, fromSlot, toContainer, toSlot );
       }
       break;
 
       case InventoryOperation::Split: // split stack action
       {
-
+         player.getInventory()->splitItem( fromContainer, fromSlot, toContainer, toSlot, splitCount );
       }
       break;
 
