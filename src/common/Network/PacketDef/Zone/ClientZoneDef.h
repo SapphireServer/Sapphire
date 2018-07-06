09d00b8a7f11c7f4fdbe24ef96da893e40a015a0
NotAdam <adam@notadam.me>
2018-07-06 22:43:49 +1000

    start moving old packets to new incoming packet system

--- src/common/Network/PacketDef/Zone/ClientZoneDef.h
+++ src/common/Network/PacketDef/Zone/ClientZoneDef.h
@@ -18,6 +18,34 @@ struct FFXIVIpcGmCommand1 : FFXIVIpcBasePacket< GMCommand1 >
    /* 0018 */ uint32_t param3;
 };
 
+struct FFXIVIpcGmCommand2 : FFXIVIpcBasePacket< GMCommand2 >
+{
+   /* 0000 */ uint32_t commandId;
+   /* 0004 */ char unk_4[0x10];
+   /* 0014 */ char param1[0x20];
+};
+
+struct FFXIVIpcClientTrigger : FFXIVIpcBasePacket< ClientTrigger >
+{
+   /* 0000 */ uint16_t commandId;
+   /* 0002 */ char unk_2[2];
+   /* 0004 */ uint64_t param1;
+   /* 000C */ uint32_t param2;
+   /* 0010 */ char unk_10[8];
+   /* 0018 */ uint64_t param3;
+};
+
+struct FFXIVIpcSkillHandler : FFXIVIpcBasePacket< SkillHandler >
+{
+   /* 0000 */ char pad_0000[1];
+   /* 0001 */ uint8_t type;
+   /* 0002 */ char pad_0002[2];
+   /* 0004 */ uint32_t actionId;
+   /* 0008 */ uint32_t useCount;
+   /* 000C */ char pad_000C[4];
+   /* 0010 */ uint64_t targetId;
+};
+
 }
 }
 }
