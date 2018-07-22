b54ff953884da2208ad7fd7e2d17373b810df635
NotAdam <adam@notadam.me>
2018-07-22 16:54:23 +1000

    move common effect struct members into a shared struct for single target/aoe

--- src/servers/sapphire_zone/Network/PacketWrappers/EffectPacket.h
+++ src/servers/sapphire_zone/Network/PacketWrappers/EffectPacket.h
@@ -17,25 +17,25 @@ public:
    EffectPacket( uint64_t sourceId, uint32_t targetId, uint32_t actionId ) :
       ZoneChannelPacket< FFXIVIpcEffect >( sourceId, targetId )
    {
-      m_data.actionId = actionId;
-      m_data.actionAnimationId = static_cast< uint16_t >( actionId );
+      m_data.header.actionId = actionId;
+      m_data.header.actionAnimationId = static_cast< uint16_t >( actionId );
 
-      m_data.animationTargetId = targetId;
+      m_data.header.animationTargetId = targetId;
       m_data.effectTargetId = targetId;
       
-      m_data.effectDisplayType = Common::ActionEffectDisplayType::ShowActionName;
+      m_data.header.effectDisplayType = Common::ActionEffectDisplayType::ShowActionName;
    }
 
    void addEffect( const Server::EffectEntry& effect )
    {
-      assert( m_data.effectCount <= 8 );
+      assert( m_data.header.effectCount <= 8 );
 
-      std::memcpy( &m_data.effects[m_data.effectCount++], &effect, sizeof( Server::EffectEntry ) );
+      std::memcpy( &m_data.effects[m_data.header.effectCount++], &effect, sizeof( Server::EffectEntry ) );
    }
 
    void setAnimationId( const uint16_t animationId )
    {
-      m_data.actionAnimationId = animationId;
+      m_data.header.actionAnimationId = animationId;
    }
 
    void setEffectFlags( const uint32_t effectFlags )
@@ -45,12 +45,12 @@ public:
 
    void setRotation( const uint16_t rotation )
    {
-      m_data.rotation = rotation;
+      m_data.header.rotation = rotation;
    }
 
    void setTargetActor( const uint32_t targetId )
    {
-      m_data.animationTargetId = targetId;
+      m_data.header.animationTargetId = targetId;
       m_data.effectTargetId = targetId;
 
       FFXIVPacketBase::setTargetActor( targetId );
