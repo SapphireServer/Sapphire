8eee8c28752545966763df5c63f83c0d9919df4f
NotAdam <adam@notadam.me>
2018-07-28 21:51:58 +1000

    remove bad memes

--- src/servers/sapphire_zone/Action/ActionCast.h
+++ src/servers/sapphire_zone/Action/ActionCast.h
@@ -10,18 +10,12 @@ namespace Action {
    class ActionCast : public Action
    {
    private:
-      uint32_t m_idOverride;
-
-      uint32_t getOverrideId() const;
 
    public:
       ActionCast();
       ~ActionCast() = default;
       
       ActionCast( Entity::CharaPtr pActor, Entity::CharaPtr pTarget, uint32_t actionId );
-
-      void setIdOverride( uint32_t param );
-      uint32_t getIdOverride() const;
 
       void onStart() override;
       void onFinish() override;
