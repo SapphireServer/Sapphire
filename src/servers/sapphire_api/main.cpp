58da892596253b014cccb336bddaeea3181d6d2c
mordred <you@example.com>
2018-10-26 09:29:57 +0200

    boost::hash_combine removed

--- src/servers/sapphire_api/main.cpp
+++ src/servers/sapphire_api/main.cpp
@@ -6,7 +6,6 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/property_tree/json_parser.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 #include <Logging/Logger.h>
 #include <Config/ConfigMgr.h>
 
