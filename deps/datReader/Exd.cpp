ee7cef96cc979d62f2060e7ab51400951f2ad2ed
mordred <you@example.com>
2018-11-23 15:02:42 +0100

    Added variant identification to exds and fixed exd_struct_gen with adam

--- deps/datReader/Exd.cpp
+++ deps/datReader/Exd.cpp
@@ -81,6 +81,90 @@ namespace xiv
       {
       }
 
+      const std::vector<Field> Exd::get_row( uint32_t id, uint32_t subRow )
+      {
+  
+         auto cacheEntryIt = _idCache.find( id );
+         if( cacheEntryIt == _idCache.end() )
+            throw std::runtime_error( "Id not found: " + std::to_string( id ) );
+  
+         // Iterates over all the files
+         const uint32_t member_count = _exh->get_members().size();
+         auto& file_ptr = cacheEntryIt->second.file;
+  
+         std::vector< char > dataCpy = file_ptr->get_data_sections().front();
+         std::istringstream iss( std::string( dataCpy.begin(), dataCpy.end() ) );
+  
+         // Get the vector fields for the given record and preallocate it
+         auto fields = _data[id];
+         fields.reserve( member_count );
+         iss.seekg( cacheEntryIt->second.offset + 6 );
+  
+         uint8_t subRows = *reinterpret_cast< uint8_t* >( &dataCpy[ cacheEntryIt->second.offset + 5 ] );
+  
+         for( auto& member_entry : _exh->get_exh_members() )
+         {
+              // Seek to the position of the member to extract.
+            // 6 is because we have uint32_t/uint16_t at the start of each record
+            iss.seekg( cacheEntryIt->second.offset + 6 + member_entry.offset );
+  
+            // Switch depending on the type to extract
+            switch( member_entry.type )
+            {
+            case DataType::string:
+               // Extract the offset to the actual string
+               // Seek to it then extract the actual string
+            {
+               auto string_offset = extract<uint32_t>( iss, "string_offset", false );
+               iss.seekg( cacheEntryIt->second.offset + 6 + _exh->get_header().data_offset + string_offset );
+               fields.emplace_back( utils::bparse::extract_cstring( iss, "string" ) );
+            }
+            break;
+  
+            case DataType::boolean:
+               fields.emplace_back( extract<bool>( iss, "bool" ) );
+               break;
+  
+            case DataType::int8:
+               fields.emplace_back( extract<int8_t>( iss, "int8_t" ) );
+               break;
+
+            case DataType::int16:
+               fields.emplace_back( extract<int16_t>( iss, "int16_t", false ) );
+               break;
+            case DataType::int32:
+               fields.emplace_back( extract<int32_t>( iss, "int32_t", false ) );
+               break;
+  
+            case DataType::uint32:
+               fields.emplace_back( extract<uint32_t>( iss, "uint32_t", false ) );
+               break;
+
+            case DataType::float32:
+               fields.emplace_back( extract<float>( iss, "float", false ) );
+               break;
+
+            case DataType::uint64:
+               fields.emplace_back( extract<uint64_t>( iss, "uint64_t", false ) );
+               break;
+
+            default:
+               auto type = static_cast< uint16_t >( member_entry.type );
+               if( type < 0x19 || type > 0x20 )
+                  throw std::runtime_error("Unknown DataType: " + std::to_string( type ));
+               uint64_t val = extract< uint64_t >( iss, "bool" );
+               int32_t shift = type - 0x19;
+               int32_t i = 1 << shift;
+               val &= i;
+               fields.emplace_back( ( val & i ) == i );
+               break;
+            }
+         }
+         return fields;
+
+      }
+
+
       const std::vector<Field> Exd::get_row( uint32_t id )
       {
 
@@ -98,6 +182,9 @@ namespace xiv
          // Get the vector fields for the given record and preallocate it
          auto fields = _data[id];
          fields.reserve( member_count );
+         iss.seekg( cacheEntryIt->second.offset + 6 );
+
+	 uint8_t subRows = *reinterpret_cast< uint8_t* >( &dataCpy[ cacheEntryIt->second.offset + 5 ] );
 
          for( auto& member_entry : _exh->get_exh_members() )
          {
