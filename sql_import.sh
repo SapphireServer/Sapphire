036bc5fc244b322a236d2e6b1f98a37b14dcadd9
mordred <you@example.com>
2018-12-13 10:18:51 +0100

    Actually run sapphire_dbm

--- sql_import.sh
+++ sql_import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd bin
-sapphire_dbm --mode initialize --user root --database sapphire --sfile sql/schema/schema.sql --ifile sql/schema/inserts.sql
-sapphire_dbm --mode liquidate --user root --database sapphire --force
+./sapphire_dbm --mode initialize --user root --database sapphire --sfile sql/schema/schema.sql --ifile sql/schema/inserts.sql
+./sapphire_dbm --mode liquidate --user root --database sapphire --force
 cd ..
