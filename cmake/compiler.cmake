e1b91b8664c9e6aab9e0095f58a24b024cc4cbe2
Dantestyle <null-fox@hotmail.it>
2018-12-30 02:46:35 +0100

    Avoid Cmake that create C++14 build with C++17 installed

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -9,6 +9,14 @@ else()
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHc")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj")
+  
+  if (MSVC_VERSION GREATER_EQUAL "1900")
+    include(CheckCXXCompilerFlag)
+    CHECK_CXX_COMPILER_FLAG("/std:c++latest" _cpp_latest_flag_supported)
+    if (_cpp_latest_flag_supported)
+        add_compile_options("/std:c++latest")
+    endif()
+  endif()
 
   if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     # disabling SAFESEH
