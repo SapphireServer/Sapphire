a30ae6bcac3f81ba8e0e012aecd6818653966d95
NotAdam <adam@notadam.me>
2019-01-26 19:43:42 +1100

    fix more style

--- src/tools/pcb_reader/nav/ext/MeshLoaderObj.h
+++ src/tools/pcb_reader/nav/ext/MeshLoaderObj.h
@@ -25,24 +25,50 @@ class rcMeshLoaderObj
 {
 public:
   rcMeshLoaderObj();
+
   ~rcMeshLoaderObj();
 
-  bool load(const std::string& fileName);
+  bool load( const std::string& fileName );
 
-  const float* getVerts() const { return m_verts; }
-  const float* getNormals() const { return m_normals; }
-  const int* getTris() const { return m_tris; }
-  int getVertCount() const { return m_vertCount; }
-  int getTriCount() const { return m_triCount; }
-  const std::string& getFileName() const { return m_filename; }
+  const float* getVerts() const
+  {
+    return m_verts;
+  }
+
+  const float* getNormals() const
+  {
+    return m_normals;
+  }
+
+  const int* getTris() const
+  {
+    return m_tris;
+  }
+
+  int getVertCount() const
+  {
+    return m_vertCount;
+  }
+
+  int getTriCount() const
+  {
+    return m_triCount;
+  }
+
+  const std::string& getFileName() const
+  {
+    return m_filename;
+  }
 
 private:
   // Explicitly disabled copy constructor and copy assignment operator.
-  rcMeshLoaderObj(const rcMeshLoaderObj&);
-  rcMeshLoaderObj& operator=(const rcMeshLoaderObj&);
+  rcMeshLoaderObj( const rcMeshLoaderObj& );
 
-  void addVertex(float x, float y, float z, int& cap);
-  void addTriangle(int a, int b, int c, int& cap);
+  rcMeshLoaderObj& operator=( const rcMeshLoaderObj& );
+
+  void addVertex( float x, float y, float z, int& cap );
+
+  void addTriangle( int a, int b, int c, int& cap );
 
   std::string m_filename;
   float m_scale;
