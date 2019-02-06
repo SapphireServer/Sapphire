87927b371329c5502527d267657908a8573b25f4
AriAvery <41122212+AriAvery@users.noreply.github.com>
2019-02-06 12:01:33 +0100

    changed back to old comparing

--- src/common/Vector3.h
+++ src/common/Vector3.h
@@ -1,18 +1,15 @@
 #ifndef _VECTOR3_H_
 #define _VECTOR3_H_
 
-namespace Sapphire
+namespace Sapphire::Common
 {
-  namespace Common
+  struct FFXIVARR_POSITION3
   {
-    struct FFXIVARR_POSITION3
-    {
-      float x;
-      float y;
-      float z;
-      inline bool operator == ( const FFXIVARR_POSITION3& target ) const;
-    };
-  }
+    float x;
+    float y;
+    float z;
+    inline bool operator == ( const FFXIVARR_POSITION3& target ) const;
+  };
 }
 
 #endif
