747f244c66b965cf0a1b24f5f6b4487d2ee7775e
AriAvery <41122212+AriAvery@users.noreply.github.com>
2019-02-06 12:01:33 +0100

    changed back to old comparing

--- src/common/Vector3.cpp
+++ src/common/Vector3.cpp
@@ -1,8 +1,6 @@
-#include <string.h> 
-
 #include "Vector3.h"
 
 inline bool Sapphire::Common::FFXIVARR_POSITION3::operator == ( const FFXIVARR_POSITION3& target ) const
 {
-  return !memcmp( this, &target, sizeof( FFXIVARR_POSITION3 ) );
+  return x == target.x && y == target.y && z == target.z;
 }
