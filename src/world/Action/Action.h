f0bf2426219017ba95783acd93d7900cd6074358
NotAdam <adam@notadam.me>
2019-02-08 22:09:48 +1100

    move the old actioncast code into base action class

--- src/world/Action/Action.h
+++ src/world/Action/Action.h
@@ -12,47 +12,38 @@ namespace Sapphire::Action
 
   public:
     Action();
+    Action( Entity::CharaPtr caster, Entity::CharaPtr target, uint16_t actionId, FrameworkPtr fw );
 
     virtual ~Action();
 
     uint16_t getId() const;
 
-    Common::HandleActionType getHandleActionType() const;
-
     Entity::CharaPtr getTargetChara() const;
-
-    bool isInterrupted() const;
-
-    void setInterrupted();
-
-    uint64_t getStartTime() const;
-
-    void setStartTime( uint64_t startTime );
-
-    uint32_t getCastTime() const;
-
-    void setCastTime( uint32_t castTime );
-
     Entity::CharaPtr getActionSource() const;
 
-    virtual void onStart()
-    {
-    };
+    bool isInterrupted() const;
+    void setInterrupted();
 
-    virtual void onFinish()
-    {
-    };
+    uint32_t getCastTime() const;
+    void setCastTime( uint32_t castTime );
 
-    virtual void onInterrupt()
-    {
-    };
+    /*!
+     * @brief Tests whether the action is instantly usable or has a cast assoc'd with it
+     * @return true if action has a cast time
+     */
+    bool isCastedAction() const;
+
+    void start();
+
+    virtual void onStart();
+    virtual void onFinish();
+    virtual void onInterrupt();
 
     // update action, if returns true, action is done and has to be removed from the actor
     virtual bool update();
 
   protected:
     uint16_t m_id;
-    Common::HandleActionType m_handleActionType;
 
     uint64_t m_startTime;
     uint32_t m_castTime;
