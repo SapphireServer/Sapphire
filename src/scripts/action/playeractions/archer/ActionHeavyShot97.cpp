7ecbbd07c77e5a847f3df228d9fcb89f5bc6e932
NotAdam <adam@notadam.me>
2019-02-11 14:51:56 +1100

    cleanup some unneeded log output

--- src/scripts/action/playeractions/archer/ActionHeavyShot97.cpp
+++ src/scripts/action/playeractions/archer/ActionHeavyShot97.cpp
@@ -12,13 +12,6 @@ public:
   {
   }
 
-  void onCastFinish( Sapphire::Action::Action& currentAction ) override
-  {
-    auto source = currentAction.getSourceChara();
-    if( auto player = source->getAsPlayer() )
-      player->sendDebug( "cast finished" );
-  }
-
   void onCharaHit( Sapphire::Action::Action& currentAction, Sapphire::Entity::Chara& hitActor ) override
   {
     currentAction.damageTarget( 150, hitActor );
