2bf1e3767a807d54331d32773e91e3832d118997
collett <collett0301@gmail.com>
2020-01-03 04:06:25 +0900

    Play the sound effect when buying items from shop. When selling the sound effect is also played (and shop switch to buy back tab as well) but the selling logic code is not there so...

--- src/world/Actor/PlayerEvent.cpp
+++ src/world/Actor/PlayerEvent.cpp
@@ -130,7 +130,7 @@ void Sapphire::Entity::Player::playScene( uint32_t eventId, uint32_t scene,
   playScene( eventId, scene, flags, eventParam2, eventParam3, 0, eventCallback );
 }
 
-void Sapphire::Entity::Player::playGilShop( uint32_t eventId, uint32_t flags,
+void Sapphire::Entity::Player::playGilShop( uint32_t eventId, uint32_t flags, uint32_t param,
                                             Event::EventHandler::SceneReturnCallback eventCallback )
 {
   auto pEvent = bootstrapSceneEvent( eventId, flags );
@@ -145,6 +145,8 @@ void Sapphire::Entity::Player::playGilShop( uint32_t eventId, uint32_t flags,
   openGilShopPacket->data().eventId = eventId;
   openGilShopPacket->data().sceneFlags = flags;
   openGilShopPacket->data().actorId = getId();
+  openGilShopPacket->data().params[ 0 ] = 1;
+  openGilShopPacket->data().params[ 1 ] = param;
 
   openGilShopPacket->data().scene = 10;
 
