dc562bfe61ed06e3d24947933aeeab02f4b0ba07
Mordred <ffxivsapphire@gmail.com>
2022-02-11 10:32:13 +0100

    Added layerset parsing

--- src/world/Network/PacketWrappers/InitZonePacket.h
+++ src/world/Network/PacketWrappers/InitZonePacket.h
@@ -23,10 +23,13 @@ namespace Sapphire::Network::Packets::WorldPackets::Server
     {
       m_data.ZoneId = territory.getTerritoryTypeId();
       m_data.TerritoryType = territory.getTerritoryTypeId();
+      m_data.TerritoryIndex = 0;
       m_data.WeatherId = static_cast< uint8_t >( territory.getCurrentWeather() );
-      m_data.Flag = 0x1;
+      m_data.Flag = 1;
       m_data.FestivalEid0 = territory.getCurrentFestival().first;
       m_data.FestivalPid0 = territory.getCurrentFestival().second;
+      //m_data.LayoutId = 85915;
+      //m_data.LayerSetId = 115133;
       m_data.Pos[ 0 ] = player.getPos().x;
       m_data.Pos[ 1 ] = player.getPos().y;
       m_data.Pos[ 2 ] = player.getPos().z;
