ebdc2dd64609fe5bf713dce82191c1b95f6d76af
dude22072 <josephmikelkeller@gmail.com>
2022-02-15 23:36:48 -0600

    Gridania MSQ: Little Ala Mhigo

---
+++ src/scripts/quest/subquest/thanalan_south/GaiUsa710.cpp
@@ -0,0 +1,102 @@
+// This is an automatically generated C++ script template
+// Content needs to be added by hand to make it function
+// In order for this script to be loaded, move it to the correct folder in <root>/scripts/
+
+#include "Manager/EventMgr.h"
+#include <Actor/Player.h>
+#include <ScriptObject.h>
+#include <Service.h>
+
+// Quest Script: GaiUsa710_00783
+// Quest Name: Back to Square One
+// Quest ID: 66319
+// Start NPC: 1006215
+// End NPC: 1006688
+
+using namespace Sapphire;
+
+class GaiUsa710 : public Sapphire::ScriptAPI::QuestScript
+{
+private:
+  // Basic quest information
+  // Quest vars / flags used
+  // UI8AL
+
+  /// Countable Num: 0 Seq: 255 Event: 1 Listener: 1006688
+  // Steps in this quest ( 0 is before accepting,
+  // 1 is first, 255 means ready for turning it in
+  enum Sequence : uint8_t
+  {
+    Seq0 = 0,
+    SeqFinish = 255,
+  };
+
+  // Entities found in the script data of the quest
+  static constexpr auto Actor0 = 1006215;//Gundobland
+  static constexpr auto Actor1 = 1006688;//Minfilia
+  static constexpr auto LocAction0 = 811;
+  static constexpr auto LocActor0 = 1007817;
+  static constexpr auto LocPosActor0 = 4148563;
+
+public:
+  GaiUsa710() : Sapphire::ScriptAPI::QuestScript( 66319 ){};
+  ~GaiUsa710() = default;
+
+  //////////////////////////////////////////////////////////////////////
+  // Event Handlers
+  void onTalk( World::Quest& quest, Entity::Player& player, uint64_t actorId ) override
+  {
+    switch( actorId )
+    {
+      case Actor0:
+      {
+        if( quest.getSeq() == Seq0 )
+          Scene00000( quest, player );
+        break;
+      }
+      case Actor1:
+      {
+        if( quest.getSeq() == SeqFinish )
+          Scene00001( quest, player );
+        break;
+      }
+    }
+  }
+
+
+private:
+  //////////////////////////////////////////////////////////////////////
+  // Available Scenes in this quest, not necessarly all are used
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00000( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 0, NONE, bindSceneReturn( &GaiUsa710::Scene00000Return ) );
+  }
+
+  void Scene00000Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+    if( result.getResult( 0 ) == 1 )// accept quest
+    {
+      quest.setSeq( SeqFinish );
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00001( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 1, FADE_OUT | CONDITION_CUTSCENE | HIDE_UI, bindSceneReturn( &GaiUsa710::Scene00001Return ) );
+  }
+
+  void Scene00001Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+
+    if( result.getResult( 0 ) == 1 )
+    {
+      player.finishQuest( getId() );
+    }
+  }
+};
+
+EXPOSE_SCRIPT( GaiUsa710 );
