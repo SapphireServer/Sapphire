33bb3a8f2813496e18519c5052e645be279f8b71
dude22072 <josephmikelkeller@gmail.com>
2022-02-20 17:59:39 -0600

    Few more MSQ

---
+++ src/scripts/quest/subquest/coerthas_central/GaiUsb607.cpp
@@ -0,0 +1,323 @@
+// This is an automatically generated C++ script template
+// Content needs to be added by hand to make it function
+// In order for this script to be loaded, move it to the correct folder in <root>/scripts/
+
+#include "Manager/EventMgr.h"
+#include <Actor/Player.h>
+#include <ScriptObject.h>
+#include <Service.h>
+
+#include "Actor/BNpc.h"
+#include "Manager/TerritoryMgr.h"
+#include "Territory/Territory.h"
+
+// Quest Script: GaiUsb607_00889
+// Quest Name: Three for Three
+// Quest ID: 66425
+// Start NPC: 1006380
+// End NPC: 1006380
+
+using namespace Sapphire;
+
+class GaiUsb607 : public Sapphire::ScriptAPI::QuestScript
+{
+private:
+  // Basic quest information
+  // Quest vars / flags used
+  // BitFlag8
+  // UI8AH
+  // UI8AL
+  // UI8BH
+  // UI8BL
+  // UI8CH
+  // UI8CL
+  // UI8DH
+
+  /// Countable Num: 0 Seq: 1 Event: 10 Listener: 5000000
+  /// Countable Num: 0 Seq: 255 Event: 1 Listener: 2002261
+  // Steps in this quest ( 0 is before accepting,
+  // 1 is first, 255 means ready for turning it in
+  enum Sequence : uint8_t
+  {
+    Seq0 = 0,
+    Seq1 = 1,
+    SeqFinish = 255,
+  };
+
+  // Entities found in the script data of the quest
+  static constexpr auto Actor0 = 1006380;//Portelaine
+  static constexpr auto Enemy0 = 4290078;
+  static constexpr auto Enemy1 = 4290079;
+  static constexpr auto Enemy2 = 4290080;
+  static constexpr auto Eobject0 = 2002261;//Stolen Wares (Southern)
+  static constexpr auto Eobject1 = 2002111;//Stolen Wares (Northern)
+  static constexpr auto Eobject2 = 2002110;//Stolen Wares (Center)
+  static constexpr auto Eventrange0 = 4290068;
+  static constexpr auto EventActionSearch = 1;
+  static constexpr auto Item0 = 2000693;
+  static constexpr auto Item1 = 2000694;
+  static constexpr auto Item2 = 2000695;
+  static constexpr auto LocActor0 = 1007664;
+  static constexpr auto LocActor1 = 1006382;
+  static constexpr auto LocBgm1 = 130;
+  static constexpr auto LocEobj1 = 2002556;
+  static constexpr auto LocEobj2 = 2002557;
+  static constexpr auto LocEobj3 = 2002558;
+  static constexpr auto LocPosActor1 = 4260909;
+  static constexpr auto LocPosEobj1 = 4329703;
+  static constexpr auto LocPosEobj2 = 4329704;
+  static constexpr auto LocPosEobj3 = 4329705;
+
+public:
+  GaiUsb607() : Sapphire::ScriptAPI::QuestScript( 66425 ){};
+  ~GaiUsb607() = default;
+
+  //////////////////////////////////////////////////////////////////////
+  // Event Handlers
+  void onTalk( World::Quest& quest, Entity::Player& player, uint64_t actorId ) override
+  {
+    switch( actorId )
+    {
+      case Actor0:
+      {
+        if( quest.getSeq() == Seq0 )
+          Scene00000( quest, player );
+        else if( quest.getSeq() == SeqFinish )
+          Scene00009( quest, player );
+        break;
+      }
+      case Eobject0:
+      {
+        if( quest.getUI8BL() < 3 )
+          Scene00003( quest, player );
+        else
+          eventMgr().eventActionStart(
+                  player, getId(), EventActionSearch,
+                  [ & ]( Entity::Player& player, uint32_t eventId, uint64_t additional ) {
+                    Scene00004( quest, player );
+                  },
+                  nullptr, 0 );
+        break;
+      }
+      case Eobject1:
+      {
+        eventMgr().eventActionStart(
+                player, getId(), EventActionSearch,
+                [ & ]( Entity::Player& player, uint32_t eventId, uint64_t additional ) {
+                  Scene00005( quest, player );
+                },
+                nullptr, 0 );
+        break;
+      }
+      case Eobject2:
+      {
+        eventMgr().eventActionStart(
+                player, getId(), EventActionSearch,
+                [ & ]( Entity::Player& player, uint32_t eventId, uint64_t additional ) {
+                  Scene00007( quest, player );
+                },
+                nullptr, 0 );
+        break;
+      }
+    }
+  }
+
+  void onBNpcKill( World::Quest& quest, uint16_t nameId, uint32_t entityId, Entity::Player& player ) override
+  {
+    switch( entityId )
+    {
+      case Enemy0:
+      case Enemy1:
+      case Enemy2:
+      {
+        quest.setUI8BL( quest.getUI8BL() + 1 );
+        break;
+      }
+    }
+  }
+
+  void onWithinRange( World::Quest& quest, Sapphire::Entity::Player& player, uint32_t eventId, uint32_t param1, float x, float y, float z ) override
+  {
+    if( param1 == Eventrange0 )
+      Scene00002( quest, player );
+  }
+
+private:
+  void checkQuestCompletion( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().sendEventNotice( player, getId(), 0, 2, quest.getUI8AH(), 3 );
+    if( quest.getUI8AH() >= 3 )
+    {
+      quest.setUI8BH( quest.getUI8CH() );
+      quest.setUI8BL( quest.getUI8CL() );
+      quest.setUI8CH( quest.getUI8DH() );
+      quest.setSeq( SeqFinish );
+    }
+  }
+  //////////////////////////////////////////////////////////////////////
+  // Available Scenes in this quest, not necessarly all are used
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00000( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 0, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00000Return ) );
+  }
+
+  void Scene00000Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+    if( result.getResult( 0 ) == 1 )// accept quest
+    {
+      Scene00001( quest, player );
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00001( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 1, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00001Return ) );
+  }
+
+  void Scene00001Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+    quest.setSeq( Seq1 );
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00002( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 2, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00002Return ) );
+  }
+
+  void Scene00002Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+    auto instance = teriMgr().getTerritoryByGuId( player.getTerritoryId() );
+
+    auto enemy0spawned = instance->getActiveBNpcByInstanceIdAndTriggerOwner( Enemy0, player.getId() );
+    auto enemy1spawned = instance->getActiveBNpcByInstanceIdAndTriggerOwner( Enemy1, player.getId() );
+    auto enemy2spawned = instance->getActiveBNpcByInstanceIdAndTriggerOwner( Enemy2, player.getId() );
+
+    if( !enemy0spawned && !enemy1spawned && !enemy2spawned && quest.getUI8BL() < 3 )
+    {
+      auto enemy0 = instance->createBNpcFromInstanceId( Enemy0, 1220 /*Find the right value*/, Common::BNpcType::Enemy, player.getId() );
+      auto enemy1 = instance->createBNpcFromInstanceId( Enemy1, 1220 /*Find the right value*/, Common::BNpcType::Enemy, player.getId() );
+      auto enemy2 = instance->createBNpcFromInstanceId( Enemy2, 1220 /*Find the right value*/, Common::BNpcType::Enemy, player.getId() );
+
+      enemy0->hateListAddDelayed( player.getAsPlayer(), 1 );
+      enemy1->hateListAddDelayed( player.getAsPlayer(), 1 );
+      enemy2->hateListAddDelayed( player.getAsPlayer(), 1 );
+    }
+
+    
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00003( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 3, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00003Return ) );
+  }
+
+  void Scene00003Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00004( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 4, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00004Return ) );
+  }
+
+  void Scene00004Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+    quest.setBitFlag8( 1, true );
+    quest.setUI8AH( quest.getUI8AH() + 1 );
+    quest.setUI8CH( 1 );
+    checkQuestCompletion( quest, player );
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00005( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 5, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00005Return ) );
+  }
+
+  void Scene00005Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+    quest.setBitFlag8( 2, true );
+    quest.setUI8AH( quest.getUI8AH() + 1 );
+    quest.setUI8CL( 1 );
+    checkQuestCompletion( quest, player );
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00006( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 6, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00006Return ) );
+  }
+
+  void Scene00006Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00007( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 7, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00007Return ) );
+  }
+
+  void Scene00007Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+    quest.setBitFlag8( 3, true );
+    quest.setUI8AH( quest.getUI8AH() + 1 );
+    quest.setUI8DH( 1 );
+    checkQuestCompletion( quest, player );
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00008( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 8, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00008Return ) );
+  }
+
+  void Scene00008Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00009( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 9, HIDE_HOTBAR, bindSceneReturn( &GaiUsb607::Scene00009Return ) );
+  }
+
+  void Scene00009Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+    if( result.getResult( 0 ) == 1 )
+      Scene00010( quest, player );
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Scene00010( World::Quest& quest, Entity::Player& player )
+  {
+    eventMgr().playQuestScene( player, getId(), 10, FADE_OUT | CONDITION_CUTSCENE | HIDE_UI, bindSceneReturn( &GaiUsb607::Scene00010Return ) );
+  }
+
+  void Scene00010Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
+  {
+
+    if( result.getResult( 0 ) == 1 )
+    {
+      player.finishQuest( getId() );
+    }
+  }
+};
+
+EXPOSE_SCRIPT( GaiUsb607 );
--- src/scripts/quest/subquest/coerthas_central/GaiUsb702.cpp
+++ src/scripts/quest/subquest/coerthas_central/GaiUsb702.cpp
@@ -7,43 +7,46 @@
 #include <ScriptObject.h>
 #include <Service.h>
 
-// Quest Script: GaiUsb605_00887
-// Quest Name: Men of Honor
-// Quest ID: 66423
-// Start NPC: 1006378
-// End NPC: 1006380
+// Quest Script: GaiUsb702_00897
+// Quest Name: The Talk of Coerthas
+// Quest ID: 66433
+// Start NPC: 1006384
+// End NPC: 1006384
 
 using namespace Sapphire;
 
-class GaiUsb605 : public Sapphire::ScriptAPI::QuestScript
+class GaiUsb702 : public Sapphire::ScriptAPI::QuestScript
 {
 private:
   // Basic quest information
   // Quest vars / flags used
+  // BitFlag8
   // UI8AL
+  // UI8BH
+  // UI8BL
 
-  /// Countable Num: 0 Seq: 1 Event: 1 Listener: 1006379
-  /// Countable Num: 0 Seq: 2 Event: 1 Listener: 1006378
-  /// Countable Num: 0 Seq: 255 Event: 1 Listener: 1006380
+  /// Countable Num: 0 Seq: 1 Event: 1 Listener: 1006395
+  /// Countable Num: 0 Seq: 1 Event: 1 Listener: 1006396
+  /// Countable Num: 0 Seq: 1 Event: 1 Listener: 1006377
+  /// Countable Num: 0 Seq: 255 Event: 1 Listener: 1006384
   // Steps in this quest ( 0 is before accepting,
   // 1 is first, 255 means ready for turning it in
   enum Sequence : uint8_t
   {
     Seq0 = 0,
     Seq1 = 1,
-    Seq2 = 2,
     SeqFinish = 255,
   };
 
   // Entities found in the script data of the quest
-  static constexpr auto Actor0 = 1006378;//Jocea
-  static constexpr auto Actor1 = 1006379;//Established Astrologian 
-  static constexpr auto Actor2 = 1006380;//Portelaine
-  static constexpr auto EventActionRescueUnderMiddle = 35;
+  static constexpr auto Actor0 = 1006384;//Haurchefant
+  static constexpr auto Actor1 = 1006395;//Ninne
+  static constexpr auto Actor2 = 1006396;//Cravellin
+  static constexpr auto Actor3 = 1006377;//Forlemort
 
 public:
-  GaiUsb605() : Sapphire::ScriptAPI::QuestScript( 66423 ){};
-  ~GaiUsb605() = default;
+  GaiUsb702() : Sapphire::ScriptAPI::QuestScript( 66433 ){};
+  ~GaiUsb702() = default;
 
   //////////////////////////////////////////////////////////////////////
   // Event Handlers
@@ -55,25 +58,26 @@ public:
       {
         if( quest.getSeq() == Seq0 )
           Scene00000( quest, player );
-        else if( quest.getSeq() == Seq2 )
-          Scene00004( quest, player );
+        else if( quest.getSeq() == SeqFinish )
+          Scene00005( quest, player );
         break;
       }
       case Actor1:
       {
         if( quest.getSeq() == Seq1 )
-          eventMgr().eventActionStart(
-                  player, getId(), EventActionRescueUnderMiddle,
-                  [ & ]( Entity::Player& player, uint32_t eventId, uint64_t additional ) {
-                    Scene00003( quest, player );
-                  },
-                  nullptr, 0 );
+          Scene00002( quest, player );
         break;
       }
       case Actor2:
       {
-        if( quest.getSeq() == SeqFinish )
-          Scene00005( quest, player );
+        if( quest.getSeq() == Seq1 )
+          Scene00003( quest, player );
+        break;
+      }
+      case Actor3:
+      {
+        if( quest.getSeq() == Seq1 )
+          Scene00004( quest, player );
         break;
       }
     }
@@ -81,13 +85,23 @@ public:
 
 
 private:
+  void checkQuestCompletion( World::Quest& quest, Entity::Player& player )
+  {
+    if( quest.getUI8AL() >= 1 && quest.getUI8BH() >= 1 && quest.getUI8BL() >= 1 )
+    {
+      quest.setUI8AL( 0 );
+      quest.setUI8BH( 0 );
+      quest.setUI8BL( 0 );
+      quest.setSeq( SeqFinish );
+    }
+  }
   //////////////////////////////////////////////////////////////////////
   // Available Scenes in this quest, not necessarly all are used
   //////////////////////////////////////////////////////////////////////
 
   void Scene00000( World::Quest& quest, Entity::Player& player )
   {
-    eventMgr().playQuestScene( player, getId(), 0, HIDE_HOTBAR, bindSceneReturn( &GaiUsb605::Scene00000Return ) );
+    eventMgr().playQuestScene( player, getId(), 0, HIDE_HOTBAR, bindSceneReturn( &GaiUsb702::Scene00000Return ) );
   }
 
   void Scene00000Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
@@ -102,7 +116,7 @@ private:
 
   void Scene00001( World::Quest& quest, Entity::Player& player )
   {
-    eventMgr().playQuestScene( player, getId(), 1, HIDE_HOTBAR, bindSceneReturn( &GaiUsb605::Scene00001Return ) );
+    eventMgr().playQuestScene( player, getId(), 1, HIDE_HOTBAR, bindSceneReturn( &GaiUsb702::Scene00001Return ) );
   }
 
   void Scene00001Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
@@ -114,44 +128,49 @@ private:
 
   void Scene00002( World::Quest& quest, Entity::Player& player )
   {
-    eventMgr().playQuestScene( player, getId(), 2, HIDE_HOTBAR, bindSceneReturn( &GaiUsb605::Scene00002Return ) );
+    eventMgr().playQuestScene( player, getId(), 2, HIDE_HOTBAR, bindSceneReturn( &GaiUsb702::Scene00002Return ) );
   }
 
   void Scene00002Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
   {
+    quest.setUI8AL( 1 );
+    eventMgr().sendEventNotice( player, getId(), 0, 0 );
+    checkQuestCompletion( quest, player );
   }
 
   //////////////////////////////////////////////////////////////////////
 
   void Scene00003( World::Quest& quest, Entity::Player& player )
   {
-    eventMgr().playQuestScene( player, getId(), 3, HIDE_HOTBAR, bindSceneReturn( &GaiUsb605::Scene00003Return ) );
+    eventMgr().playQuestScene( player, getId(), 3, HIDE_HOTBAR, bindSceneReturn( &GaiUsb702::Scene00003Return ) );
   }
 
   void Scene00003Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
   {
-    eventMgr().sendEventNotice( player, getId(), 0, 0 );
-    quest.setSeq( Seq2 );
+    quest.setUI8BH( 1 );
+    eventMgr().sendEventNotice( player, getId(), 1, 0 );
+    checkQuestCompletion( quest, player );
   }
 
   //////////////////////////////////////////////////////////////////////
 
   void Scene00004( World::Quest& quest, Entity::Player& player )
   {
-    eventMgr().playQuestScene( player, getId(), 4, HIDE_HOTBAR, bindSceneReturn( &GaiUsb605::Scene00004Return ) );
+    eventMgr().playQuestScene( player, getId(), 4, HIDE_HOTBAR, bindSceneReturn( &GaiUsb702::Scene00004Return ) );
   }
 
   void Scene00004Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
   {
-    eventMgr().sendEventNotice( player, getId(), 1, 0 );
-    quest.setSeq( SeqFinish );
+    quest.setUI8BL( 1 );
+    eventMgr().sendEventNotice( player, getId(), 2, 0 );
+    checkQuestCompletion( quest, player );
   }
 
   //////////////////////////////////////////////////////////////////////
 
   void Scene00005( World::Quest& quest, Entity::Player& player )
   {
-    eventMgr().playQuestScene( player, getId(), 5, HIDE_HOTBAR, bindSceneReturn( &GaiUsb605::Scene00005Return ) );
+    eventMgr().playQuestScene( player, getId(), 5, HIDE_HOTBAR, bindSceneReturn( &GaiUsb702::Scene00005Return ) );
   }
 
   void Scene00005Return( World::Quest& quest, Entity::Player& player, const Event::SceneResult& result )
@@ -159,9 +178,9 @@ private:
 
     if( result.getResult( 0 ) == 1 )
     {
-        player.finishQuest( getId(), result.getResult(1) );
+      player.finishQuest( getId() );
     }
   }
 };
 
-EXPOSE_SCRIPT( GaiUsb605 );
+EXPOSE_SCRIPT( GaiUsb702 );
