6bd8dee22d4fc4c1237033dd73f7fc84df427b93
Alice Ogeda <alice.ogeda.s@gmail.com>
2023-03-24 11:21:09 -0300

    EncounterFight and state refactor;

--- src/world/Encounter/InstanceContent/IfritNormal.h
+++ src/world/Encounter/InstanceContent/IfritNormal.h
@@ -2,98 +2,13 @@
 
 namespace Sapphire
 {
-  class IfritNormalData
-  {
-  public:
-    static constexpr int IFRIT = 4126276;
-    static constexpr int HELLFIRE = 0;
-  };
-
-  class IfritStateTwo : public EncounterState
-  {
-  public:
-    IfritStateTwo( EncounterFightPtr pEncounter ) : EncounterState( pEncounter )
-    {
-    }
-
-    void init() override
-    {
-      Logger::info( "stage 2 init" );
-    }
-
-    void update( uint64_t deltaTime ) override
-    {
-      if( m_startTime == 0 )
-        m_startTime = deltaTime;
-
-      auto timeElapsedMs = deltaTime - m_startTime;
-
-      auto pIfrit = m_pEncounter->getBNpc( IfritNormalData::IFRIT );
-
-      pIfrit->setRot( pIfrit->getRot() - .2f );
-      pIfrit->sendPositionUpdate();
-
-      if( timeElapsedMs > 5000 )
-      {
-        m_bShouldFinish = true;
-      }
-    }
-
-    void finish() override
-    {
-      Logger::info( "stage 2 done, going back to stage 1" );
-    }
-  };
-
-
-  class IfritStateOne : public EncounterState
-  {
-  public:
-    IfritStateOne( EncounterFightPtr pEncounter ) : EncounterState( pEncounter )
-    {
-    }
-
-    void init() override
-    {
-      Logger::info( "stage 1 init" );
-    }
-
-    void update( uint64_t deltaTime ) override
-    {
-      if( m_startTime == 0 )
-        m_startTime = deltaTime;
-
-      auto timeElapsedMs = deltaTime - m_startTime;
-
-      auto pIfrit = m_pEncounter->getBNpc( IfritNormalData::IFRIT );
-
-      pIfrit->setRot( pIfrit->getRot() + .2f );
-      pIfrit->sendPositionUpdate();
-
-      if( timeElapsedMs > 10000 )
-      {
-        m_bShouldFinish = true;
-        return;
-      }
-
-      if( timeElapsedMs > 5000 )
-      {
-        auto ifritTwoState = std::make_shared< IfritStateTwo >( m_pEncounter );
-        m_pEncounter->addState( ifritTwoState );
-      }
-    }
-
-    void finish() override
-    {
-      Logger::info( "stage 1 finish - enrage" );
-
-      auto pIfrit = m_pEncounter->getBNpc( IfritNormalData::IFRIT );
-      pIfrit->hateListGetHighest()->die();
-    }
-  };
-
   class IfritEncounterFight : public EncounterFight
   {
+  private:
+    static constexpr int NPC_IFRIT = 4126276;
+    static constexpr int VAL_IFRIT_HP = 13884;
+    static constexpr int ACT_HELLFIRE = 0;
+
   public:
     IfritEncounterFight( InstanceContentPtr pInstance ) : EncounterFight( pInstance )
     {
@@ -108,35 +23,97 @@ namespace Sapphire
       m_stateStack = std::make_shared< EncounterState::StateStack >();
 
       // todo: i don't like this
-      auto boss = m_pInstance->createBNpcFromLayoutId( IfritNormalData::IFRIT, 13884, Common::BNpcType::Enemy );
+      auto boss = m_pInstance->createBNpcFromLayoutId( NPC_IFRIT, VAL_IFRIT_HP, Common::BNpcType::Enemy );
       addBNpc( boss );
-
-      //instance.sendForward();
-      /*
-      auto ifritStateTwo = std::make_shared< IfritStateTwo >( m_stateStack );
-      m_stateStack->push( ifritStateTwo );*/
-    }
-
-    void addState( EncounterState::EncounterStatePtr pState, bool initState = true ) override
-    {
-      m_stateStack->push( pState );
-      if( initState )
-        pState->init();
     }
 
     void start() override
     {
-      auto ifritInitState = std::make_shared< IfritStateOne >( shared_from_this() );
+      auto ifritInitState = makeIfritPhaseOneState();
+
       addState( ifritInitState );
 
       m_status = EncounterFightStatus::ACTIVE;
+    }
+    
+    void reset() override
+    {
+      if( auto boss = m_pInstance->getActiveBNpcByLayoutId( NPC_IFRIT ); boss )
+      {
+        removeBNpc( NPC_IFRIT );
+        m_pInstance->removeActor( boss );
+      }
+
+      init();
+    }
+
+    EncounterStatePtr makeIfritPhaseOneState()
+    {
+      auto ifritInitState = std::make_shared< EncounterState >( shared_from_this() );
+      ifritInitState->setOnUpdateCallback( [ & ]( EncounterFightPtr pEncounter, EncounterState state )
+        {
+          auto timeElapsedMs = state.getElapsedTime();
+
+          auto pIfrit = pEncounter->getBNpc( NPC_IFRIT );
+
+          pIfrit->setRot( pIfrit->getRot() + .2f );
+          pIfrit->sendPositionUpdate();
+
+          // todo: use gambits+timelines for this
+          if( timeElapsedMs > 10000 )
+          {
+            state.setFinishFlag();
+            return;
+          }
+
+          // todo: use gambits+timelines for this
+          if( timeElapsedMs > 5000 )
+          {
+            auto ifritTwoState = makeIfritPhaseTwoState();
+            pEncounter->addState( ifritTwoState );
+          }
+        }
+      );
+
+      ifritInitState->setOnFinishCallback( [ & ]( EncounterFightPtr pEncounter, EncounterState state )
+        {
+          Logger::info( "stage 1 finish - enrage" );
+
+          auto pIfrit = pEncounter->getBNpc( NPC_IFRIT );
+          pIfrit->hateListGetHighest()->die();
+        }
+      );
+
+      return ifritInitState;
+    }
+
+    EncounterStatePtr makeIfritPhaseTwoState()
+    {
+      auto ifritTwoState = std::make_shared< EncounterState >( shared_from_this() );
+      ifritTwoState->setOnUpdateCallback( [ & ]( EncounterFightPtr pEncounter, EncounterState state ) {
+        auto timeElapsedMs = state.getElapsedTime();
+
+        auto pIfrit = pEncounter->getBNpc( NPC_IFRIT );
+
+        pIfrit->setRot( pIfrit->getRot() - .2f );
+        pIfrit->sendPositionUpdate();
+
+        // todo: use gambits+timelines for this
+        if( timeElapsedMs > 5000 )
+        {
+          state.setFinishFlag();
+        }
+      } );
+
+      return ifritTwoState;
     }
 
     void update( uint64_t deltaTime ) override
     {
       // todo: better way to start fights here..
+      // this probably doesn't need to be overriden either
 
-      auto ifrit = getBNpc( IfritNormalData::IFRIT );
+      auto ifrit = getBNpc( NPC_IFRIT );
 
       if( ifrit; ifrit->hateListGetHighestValue() != 0 && m_status == EncounterFightStatus::IDLE )
       {
@@ -160,43 +137,6 @@ namespace Sapphire
         if( !m_stateStack->empty() )
           m_stateStack->top()->update( deltaTime );
       }
-    }
-
-    void reset() override
-    {
-      auto boss = m_pInstance->getActiveBNpcByLayoutId( IfritNormalData::IFRIT );
-      if( boss )
-      {
-        removeBNpc( IfritNormalData::IFRIT );
-        m_pInstance->removeActor( boss );
-        
-      }
-
-      init();
-    }
-
-    EncounterFightStatus getEncounterFightStatus() const override
-    {
-      return m_status;
-    }
-
-    void addBNpc( Entity::BNpcPtr pBNpc ) override
-    {
-      m_bnpcs[ pBNpc->getLayoutId() ] = pBNpc;
-    }
-
-    Entity::BNpcPtr getBNpc( uint32_t layoutId ) override
-    {
-      auto bnpc = m_bnpcs.find( layoutId );
-      if( bnpc != std::end( m_bnpcs ) )
-        return bnpc->second;
-
-      return nullptr;
-    }
-
-    void removeBNpc( uint32_t layoutId ) override
-    {
-       m_bnpcs.erase( layoutId );
     }
   };
 }
